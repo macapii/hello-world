188a777c2e05c1576ee7bea950fcedc7c3ee4be5
Konata400 <34619567+Konata400@users.noreply.github.com>
2022-03-23 01:29:01 +0100

    change for php 8.1

--- install/install3.php
+++ install/install3.php
@@ -224,7 +224,7 @@ require_once("../template/errorreport.php");
         fwrite($file, "php_value max_execution_time 600" . PHP_EOL);
         fwrite($file, "php_value max_input_time 600" . PHP_EOL);
         fwrite($file, "</IfModule>" . PHP_EOL);
-        fwrite($file, "<IfModule mod_php8.c>" . PHP_EOL);
+        fwrite($file, "<IfModule mod_php.c>" . PHP_EOL);
         fwrite($file, $linea1 . PHP_EOL);
         fwrite($file, $linea2 . PHP_EOL);
         fwrite($file, $linea3 . PHP_EOL);
