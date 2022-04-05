f3b2fe87feacf248c568c2cacb8b2d50c8738776
Konata400 <34619567+Konata400@users.noreply.github.com>
2022-04-05 20:27:06 +0200

    add version of panel in constant

---
+++ errorreport.php
@@ -0,0 +1,25 @@
+<?php
+/*
+This file is part of McWebPanel.
+Copyright (C) 2020-2022 Cristina Ibañez, Konata400
+
+    McWebPanel is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    McWebPanel is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with McWebPanel.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+define("VERSIONPANEL", "0.18");
+define("VERSIONTIPO", "Develop");
+
+error_reporting(E_ALL);
+ini_set("display_errors", 0);
+?>
