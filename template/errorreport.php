ef3a61ce2a7a57a40f26d5da2bf917a4c1377c5d
Konata400 <34619567+Konata400@users.noreply.github.com>
2022-07-16 22:20:18 +0200

    Start 0.20

--- template/errorreport.php
+++ template/errorreport.php
@@ -17,8 +17,8 @@ Copyright (C) 2020-2022 Cristina Ibañez, Konata400
     along with McWebPanel.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-define("VERSIONPANEL", "0.19");
-define("VERSIONTIPO", "Pre-Release");
+define("VERSIONPANEL", "0.20");
+define("VERSIONTIPO", "Develop");
 
 error_reporting(E_ALL);
 ini_set("display_errors", 0);
